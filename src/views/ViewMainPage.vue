02501a8a39f66b558b327aa061bc2b0e85616b25
secyo <sametbirol34@gmail.com>
2023-03-02 15:15:17 +0300

    emirb branch commit and files

---
+++ src/views/ViewMainPage.vue
@@ -0,0 +1,19 @@
+<template>
+
+	
+
+	<v-container class="bg-grey-lighten-2">
+		
+		<PostCard />
+
+	</v-container>
+
+</template>
+
+<script setup>
+
+import PostCard from '@/components/postcarda.vue'
+
+</script>
+
+
