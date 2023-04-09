fdfbf6446dbcc8e85e0b3a45ff6a496303b83136
secyo <75724488+secyo@users.noreply.github.com>
2023-04-09 22:46:01 +0300

    right before firebase injection (#17)

--- backend/src/routers/auth.py
+++ backend/src/routers/auth.py
@@ -140,7 +140,7 @@ async def login_for_access_token(response: Response, form_data,
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
         return False
-    token_expires = timedelta(minutes=10)
+    token_expires = timedelta(minutes=30)
     token = create_access_token(
         user.username, user.id, expires_delta=token_expires)
     response.set_cookie(key="access_token", value=token, httponly=True)
