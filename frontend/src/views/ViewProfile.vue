99133bcf555adb8659dd9cccc9f09862f8cb52ea
sametbirol <sametbirol34@gmail.com>
2023-08-06 15:50:15 +0300

    update profile view for a wider view

--- frontend/src/views/ViewProfile.vue
+++ frontend/src/views/ViewProfile.vue
@@ -1,12 +1,9 @@
 <template>
-    <v-container class="ma-15">
-
-        <Sfallow />
-
-        <v-card clasS="ma-15 mx-auto" max-width="800" rounded="0">
-            <v-img height="100%" cover class="align-end"
+    <v-container class="">
+        <v-card clasS="mx-auto" max-width="1600" rounded="0">
+            <v-img height="300px" cover class="align-end"
                 src="https://png.pngtree.com/thumb_back/fh260/background/20200714/pngtree-modern-double-color-futuristic-neon-background-image_351866.jpg">
-                <v-avatar image="@/assets/cat2-yatay.png" size="200" class="ml-2"></v-avatar>
+                <v-avatar image="@/assets/cat2-yatay.png" size="150" class="ml-2 ma-15"></v-avatar>
 
                 <v-card-title class="text-white text-h5">Welcome,my liege!</v-card-title>
             </v-img>
@@ -16,7 +13,6 @@
     </v-container>
 </template>
 <script setup>
-import Sfallow from '@/components/sfallow.vue';
 import Tabs from '@/components/tabs.vue';
 
 </script>
